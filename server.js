27c831267d31ad2309b99d39cc900be9e74dd0dc
Solych <pavel.solovei@yandex.ru>
2018-05-06 20:26:51 +0300

    last commit

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'root',
+    database: 'chat'
+});
 
 app.get('/', function(req, res, next) {
-	res.sendFile(__dirname + '/public/index.html')
+    res.sendFile(__dirname + '/public/index.html')
 });
 
 app.use(express.static('public'));
@@ -12,15 +19,17 @@ app.use(express.static('public'));
 
 io.on('connection', function(client) {
 
-	client.on('join', function(data) {
-		console.log(data);
-	});
+    client.on('messages', function(data,name){
+        const newMessage = {name: name, message: data};
+        connection.connect(function (err) {
+            if (err) throw err;
+            console.log("CONNECTED");
+            connection.query('INSERT INTO messages SET ?',newMessage);
+        });
 
-	client.on('messages', function(data, name){
-		client.emit('thread', data);
-		client.broadcast.emit('thread', data);
-		console.log(name);
-	});
+        client.emit('thread', data, name);
+        client.broadcast.emit('thread', data, name);
+    });
 });
 
 server.listen(7777);
